5ba58fcca3f813a8a0537d105cf82fd800fd5037
Marc <conboymarc@gmail.com>
2026-03-01 01:53:47 +0000

    first commit

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -16,25 +16,8 @@ export const About = () => {
         We at Echo Sound & Light are specialists in a wide range of audio-visual services, including sound system installation, lighting design, and event production. With decades of experience, we pride ourselves on delivering high-quality solutions tailored to meet the unique needs of each client. We are dedicated to ensuring that everything we do is executed with precision and creativity, making us a trusted partner for both residential and commercial clients. Whether you're looking to install a sound system or need sound and light equipment for an event.Echo Sound & Light has the expertise and passion to bring your vision to life.
     </p>
      
-     
-
-
-    
-
-
     </div>
-    <h3 className="text-5xl font-bold  bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center py-7">Previous Projects</h3>
     
-    <img src="964506109-1751444582_original.jpeg" alt="Previous Project" className="h-auto max-w-full rounded-lg border-1" />
-    <p>Eddie Valentines - Longford Town</p>
-    <img src="PVS-Longford-00001-1.jpg" alt="Previous Project1" className="h-auto max-w-full rounded-lg border" />
-    <p>PV's - Longford Town</p>
-    <img src="bar-rua.jpg" alt="Previous Project2" className="h-auto max-w-full rounded-lg border-1 " />
-    <p>Bar Rua - Dublin City Centre </p>
-    <img src="visitor-moonriver-01.jpg" alt="Previous Project3" className="h-auto max-w-full rounded-lg border-1" />
-    <p>Moon River - Carrick On Shannon</p>
-    
-
     </div> 
     </RevealOnScroll>
     </section>
